640ea2a69eb653dc7065ba69429df33f1de6aa93
Erick Euller Opelt <erick@opelt.dev>
2021-02-26 22:42:36 -0300

    Message repository gateway test

---
+++ src/test/java/io/opelt/messagescheduler/adapter/database/gateway/MessageRepositoryGatewayTest.java
@@ -0,0 +1,47 @@
+package io.opelt.messagescheduler.adapter.database.gateway;
+
+import io.opelt.messagescheduler.adapter.database.entity.MessageEntity;
+import io.opelt.messagescheduler.adapter.database.mapper.MessageEntityMapper;
+import io.opelt.messagescheduler.adapter.database.repository.MessageEntityRepository;
+import io.opelt.messagescheduler.domain.CreateMessage;
+import io.opelt.messagescheduler.domain.Message;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MessageRepositoryGatewayTest {
+
+    @InjectMocks
+    private MessageRepositoryGateway gateway;
+
+    @Mock
+    private MessageEntityRepository repository;
+
+    @Mock
+    private MessageEntityMapper mapper;
+
+    @Test
+    void givenACreateMessageWhenSaveThenMapAndCallRepository() {
+        var createMessage = CreateMessage.builder().build();
+        var mappedEntity = MessageEntity.builder().build();
+        var savedEntity = MessageEntity.builder().build();
+        var savedMessage = Message.builder().build();
+
+        when(mapper.from(createMessage)).thenReturn(mappedEntity);
+        when(mapper.to(savedEntity)).thenReturn(savedMessage);
+        when(repository.save(mappedEntity)).thenReturn(savedEntity);
+
+        assertThat(gateway.save(createMessage)).isEqualTo(savedMessage);
+
+        verify(mapper).from(createMessage);
+        verify(mapper).to(savedEntity);
+        verify(repository).save(mappedEntity);
+        verifyNoMoreInteractions(repository, mapper);
+    }
+}
