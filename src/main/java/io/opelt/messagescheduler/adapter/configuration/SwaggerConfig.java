09c3d1bf1798cf26967b8422b5be410b28543048
Erick Euller Opelt <erick@opelt.dev>
2021-03-04 18:12:16 -0300

    Swagger configuration

--- src/main/java/io/opelt/messagescheduler/adapter/configuration/SwaggerConfig.java
+++ src/main/java/io/opelt/messagescheduler/adapter/configuration/SwaggerConfig.java
@@ -4,26 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
 
+import lombok.Getter;
+import lombok.Setter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.regex("v1/.*"))
+                .apis(RequestHandlerSelectors.basePackage("io.opelt"))
+                .paths(PathSelectors.any())
                 .build()
                 .directModelSubstitute(Pageable.class, SwaggerPageable.class);
     }
 
+    @Getter
+    @Setter
     static class SwaggerPageable {
         private Integer page;
         private Integer size;
